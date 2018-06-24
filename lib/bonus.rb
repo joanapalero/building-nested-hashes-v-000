1a501f7253fcb6e684775d335aad6f48effaec38
Joana Palero <joana.palero24@gmail.com>
2018-06-24 15:26:23 +0000

    automatically backed up by learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -1,5 +1,8 @@
 def bonus
-  epic_tragedy = {
+  epic_tragedy[:status] = "Dead"
+  puts epic_tragedy
+  # epic_tragedy = 
+  {
    :montague => {
       :patriarch => {name: "Lord Montague", age: "53"},
       :matriarch => {name: "Lady Montague", age: "54"},
